0c0369e28f0100cd09d69279f5d7b8e8ed09094b
rmm <rob.mills@epf.ch>
2021-02-17 19:00:28 +0100

    very rough script to test injecting data to influx

---
+++ nrf_node_rh_co2/rpi/ble_central/injecter_test_fillcredentials.py
@@ -0,0 +1,46 @@
+import datetime as dt
+from influxdb import InfluxDBClient
+import argparse
+
+'''
+inject data point by point from a log file
+no error checking!
+'''
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verb', action='store_true')
+    parser.add_argument('-i', '--infile', type=str, default=None)
+    parser.add_argument('-a', '--addr', type=str,required=True)
+    parser.add_argument('-n', '--num', type=int, default=4)
+    args = parser.parse_args()
+
+    ifuser = "";
+    ifpass = "";
+    ifdb = ""
+    ifhost = "localhost";
+    ifport = 8086;
+
+    ifclient = InfluxDBClient(ifhost, ifport, ifuser, ifpass, ifdb)
+
+    print(ifclient.ping())
+    print(ifclient.get_list_measurements())
+    print(ifclient.get_list_users())
+
+
+    with open(args.infile) as f:
+        for i, line in enumerate(f.readlines()):
+            if not line.startswith("timestamp"):
+                t, seq, co2, temp, RH = [float(x) for x in line.split(',')]
+                print(i,t, seq, co2, temp, RH)
+                point = {
+                    "measurement": "scd30",
+                    "tags": { "nrf_addr": args.addr, },
+                    "time": dt.datetime.fromtimestamp(t).strftime('%Y-%m-%dT%H:%M:%SZ'),
+                    "fields": { "co2": co2, "temp": temp, "rel_hum": RH, },
+                }
+                rv = ifclient.write_points([point])
+
+
+
+
