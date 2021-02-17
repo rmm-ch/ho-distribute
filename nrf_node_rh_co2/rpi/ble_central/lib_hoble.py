1ee3f84068a2982611b2ff665217eea6ffff5a0b
rmm <rob.mills@epf.ch>
2021-02-17 09:49:05 +0100

    included further converters in ble interaction lib

--- nrf_node_rh_co2/rpi/ble_central/lib_hoble.py
+++ nrf_node_rh_co2/rpi/ble_central/lib_hoble.py
@@ -10,8 +10,12 @@ HO_SNSR_CHR_DATA_FMT = "00006101-bee5-bee5-0001-bee5bee5bee5"
 HO_SNSR_CHR_CFG_HDC  = "00007011-bee5-bee5-0001-bee5bee5bee5"
 #HO_SNSR_CHR_CFG_SCD  = "00007021-bee5-bee5-0001-bee5bee5bee5"
 
+HO_SNSR_THCO2_UUID_SCD_DATA_RAW = "00006021-bee5-bee5-0001-bee5bee5bee5"
+
 HO_TIME_SVC          = "00001001-bee5-bee5-0002-bee5bee5bee5"
 HO_TIME_SET_CHAR     = "00003001-bee5-bee5-0002-bee5bee5bee5"
+
+HO_SYS_BATLVL_CHAR   = "00005005-bee5-bee5-0002-bee5bee5bee5"
 #}}}
 
 # {{{ HO cfg interface definitions
@@ -260,3 +264,147 @@ def dump_log_line(logfile, line, prefix="#", echo=True):
         with(open(logfile, "a")) as lf:
             lf.write("{}{}{}\n".format(prefix, pre2, line))
 #}}}
+
+
+#{{{ conversion of received data to readable
+def bytes_to_u32(b, bigendian=True):
+    if bigendian:
+        u32 = (b[0] | b[1] << 8 | b[2] << 16 | b[3] << 24)
+    else:
+        u32 = (b[3] | b[2] << 8 | b[1] << 16 | b[0] << 24)
+
+    return u32
+
+def bytes_to_u16(b, bigendian=True):
+    if bigendian:
+        u16 = (b[0] | b[1] << 8 )
+    else:
+        u16 = (b[1] | b[0] << 8)
+
+    return u16
+
+def conv_bat_from_bytes(bytestr):
+    B = bytes.fromhex(bytestr)
+    # 0:4 -> timestamp
+    # 4:6 -> i_batmon
+    # 6:8 -> i_vdd
+    b_ts   = B[0:4]
+    b_bat  = B[4:6]
+    b_vdd  = B[6:8]
+
+    time_s = bytes_to_u32(b_ts)
+    i_bat = bytes_to_u16(b_bat)
+    i_vdd = bytes_to_u16(b_vdd)
+
+    #uint32_t v_meas = i_bat_mon * 3.515625f;  // 1000x larger, for simple "float" print
+    #uint32_t v_bat = v_meas * 65536 /46711;   // voltage divider
+    #uint32_t v_vdd = i_vdd * 3.515625f;       // 1000x larger
+
+
+    v_bat = (i_bat * 3.515625) * (65536.0 / 46711.0 ) * (1.0/1000.0)
+    v_vdd = i_vdd * 3.515625 / 1000.0
+
+
+
+    return (time_s, v_bat, v_vdd)
+
+def conv_scd_from_bytes(bytestr):
+    B = bytes.fromhex(bytestr)
+    # 0:4 -> timestamp
+    # 4:8 -> sequence
+    # 8:12   CO2, in float format
+    # 12:16  temp in float format
+    # 16:20  RHum in float format
+    b_ts   = B[0:4]
+    b_seq  = B[4:8]
+    b_co2  = B[8:12]
+    b_temp = B[12:16]
+    b_rh   = B[16:20]
+
+    seq = bytes_to_u32(b_seq)
+    time_s = bytes_to_u32(b_ts)
+
+    # not sure why endian change here? did I transmit in reverse?
+    #memcpy(&buffer[8], (uint32_t*)&p_tag_data->p_scd30_data->raw_CO2, 4);
+    # anyway, the data is received this way around.
+    temp = struct.unpack("<f", b_temp)[0]
+    co2  = struct.unpack("<f", b_co2)[0]
+    RH   = struct.unpack("<f", b_rh)[0]
+
+
+    return (time_s, seq, co2, temp, RH)
+
+def fmt_time(time_s, fmt='%H:%M:%S'):
+    return dt.datetime.fromtimestamp(time_s).strftime('%H:%M:%S')
+
+def str_bat_from_bytes(bytestr):
+    (time_s, v_bat, v_vdd) = conv_bat_from_bytes(bytestr)
+    s = "{}  Vbat {:.3f} Vdd {:.3f}".format(
+        fmt_time(time_s), v_bat, v_vdd)
+    return s
+    #
+
+
+def str_hdc_from_bytes(bytestr):
+    (f_temp, f_hum_pct, f_timestep, utime_s) = conv_hdc_from_bytes(bytestr)
+    s = "{}  {:+5.3f}oC {:.2f}%RH".format(
+        fmt_time(utime_s), f_temp, f_hum_pct)
+        #fmt_time(f_timestep), f_temp, f_hum_pct)
+    return s
+
+def str_scd_from_bytes(bytestr):
+    (time_s, seq, co2, temp, RH) = conv_scd_from_bytes(bytestr)
+    s = "{}  {:+5.3f}oC {:.2f}%RH {:6.2f}ppm ({})".format(
+        fmt_time(time_s), temp, RH, co2, seq)
+    return s
+
+# }}}
+
+# {{{ converters - HDC
+# - note: these are all in lib_hoble now
+def conv_hdc_T_from_bytes(bytestr):
+    # py3 only!
+    B = bytes.fromhex(bytestr)
+    u16Temp = B[0] | B[1] <<8
+    # conversion in C code with 1000x for faking floating point.
+    # ((u16T * 165 * 1000) / 65536.) - 40000
+    f_temp = ((u16Temp * 165 * 1.) / 65536.) - 40
+    return f_temp
+
+def conv_hdc_RH_from_bytes(bytestr):
+    #
+    B = bytes.fromhex(bytestr)
+    u16Hum = B[2] | B[3] << 8
+    f_hum_pct = (u16Hum * 100.0) / 65536;
+
+    return f_hum_pct
+
+def conv_hdc_ts_from_bytes(bytestr):
+    B = bytes.fromhex(bytestr)
+    u32_ts = B[4] | B[5] << 8 | B[6] << 16 | B[7] << 24;
+    return u32_ts
+
+def conv_hdc_utc_from_bytes(bytestr):
+    B = bytes.fromhex(bytestr)
+    u32_time_s = B[8] | B[9] << 8 | B[10] << 16 | B[11] << 24;
+    return (u32_time_s)
+
+def conv_hdc_from_bytes(bytestr, ty4=True):
+    f_temp     = conv_hdc_T_from_bytes(bytestr)
+    f_hum_pct  = conv_hdc_RH_from_bytes(bytestr)
+    f_timestep = conv_hdc_ts_from_bytes(bytestr)
+    if len(bytestr)>= 12:
+        #f_time_s = conv_hdc_t0_from_bytes(bytestr)
+        utime_s = conv_hdc_utc_from_bytes(bytestr)
+        if ty4:
+            return (f_temp, f_hum_pct, f_timestep, utime_s)
+
+    return (f_temp, f_hum_pct, f_timestep)
+
+def disp_hdc_from_bytes(bytestr, prefix="   "):
+    (f_temp, f_hum_pct, f_timestep) = conv_hdc_from_bytes(bytestr)
+    print("{}{:4}:{:+5.3f}oC {:.2f}%RH".format(prefix, f_timestep, f_temp, f_hum_pct))
+
+
+
+# }}}
